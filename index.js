07f4ed38ba979517c712747fe71215d85c6c1373
YoungRossi <youngrossi93@gmail.com>
2026-08-18 15:03:34 -0400

    Chrome Extension

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
-//document.getElementById("count").innerText = 5
-let count = 0
-let message = "You have tree new notifications"
+let urlArr = [] 
+let inputVal = ""
 
-function increment(){
-count += 1
-document.getElementById("count").innerText = count
-console.log(count)
-}
+const input_id = document.getElementById("input-el")
+const btn_id = document.getElementById("saveBtn")
 
-function save(){
-    console.log(count)
-    document.getElementById("save_text").innerText = "Amount of people " + count + "-"
-}
 
-function reset(){
-    count = 0
-    document.getElementById("count").innerText = count
-}
+btn_id.addEventListener("click", function(){
+    inputVal = input_id.value 
+    urlArr.push(inputVal)
+    console.log(urlArr)
+})

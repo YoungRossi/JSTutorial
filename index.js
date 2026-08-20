e0468adcd6c0b32190b80a11a6bb842e69592f8a
YoungRossi <youngrossi93@gmail.com>
2026-08-20 15:01:33 -0400

    Template String

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
-let urlArr = ["Batman", "Superman", "Wolverine"] 
-let inputVal = ""
+let urlArr = [] 
 
 const input_id = document.getElementById("input-el")
 const btn_id = document.getElementById("saveBtn")
 const ul_id = document.getElementById("ul-id")
-const body_id = document.getElementById("body-id")
+const con_id = document.getElementById("container")
 
-//maybe add content into a div
+
 
 btn_id.addEventListener("click", function(){
-    inputVal = input_id.value 
-    urlArr.push(inputVal)
+    add(input_id.value)
     logArray()
     input_id.value = ""
 })
 
-function logArray(){
-    for (let i=0; i<urlArr.length; i++){
-        let li = document.createElement("li")
-        li.textContent = urlArr[i]
-        ul_id.append(li)
-        console.log(urlArr[i])
+function add(str){
+    if(str != ""){
+        urlArr.push(str)
+    }
+}
 
+function logArray(){
+    let listItems = ""
+    for (let i=0; i<urlArr.length; i++){
+        listItems += `<li><a target='_blank' href='${urlArr[i]} '>${urlArr[i]}</a></li>`
+        ul_id.innerHTML = listItems
     }
 }
 
